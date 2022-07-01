ea8e1cd467616207dd15c6f75f381d18699e3928
Karina-Jannette <103295844+Karina-Jannette@users.noreply.github.com>
2022-07-01 14:40:30 -0500

    conexion para servidor

--- conexion.php
+++ conexion.php
@@ -1,12 +1,12 @@
 <?php 
+    // $servidor= "localhost";
+    // $usuario= "root";
+    // $password= "";
+    // $bd= "auditoria";
     $servidor= "localhost";
-    $usuario= "root";
-    $password= "";
-    $bd= "auditoria";
-    /*$servidor= "localhost";
     $usuario= "u683645102_root_usoap";
     $password= "Agencia.SCT.2021";
-    $bd= "u683645102_usoap";*/
+    $bd= "u683645102_usoap";
     $conexion= mysqli_connect($servidor,$usuario,$password,$bd);
     return $conexion;
     
