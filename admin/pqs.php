3c3c8c76d0192df6c4ccbbcdafe402d8fdb7fad5
Karina-Jannette <103295844+Karina-Jannette@users.noreply.github.com>
2022-07-04 18:07:41 -0500

    cambios

--- admin/pqs.php
+++ admin/pqs.php
@@ -30,7 +30,6 @@ $resultado = $conexion->query("SELECT * from pqs")or die ($conexion->error);
     <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap4.min.css">
     <!--<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet"/>-->
     <link rel="stylesheet" href="../dist/css/select2.css">
-    
 
 </head>
 
--- js/pqseditar.js
+++ js/pqseditar.js
@@ -164,12 +164,3 @@ $(document).ready(function(){
       });
     });  
 });
-
-$(document).ready(function(){
-  var multipleCancelButton = new Choices('#choices-multiple-remove-button', {
-     removeItemButton: true,
-     maxItemCount:5,
-     searchResultLimit:5,
-     renderChoiceLimit:5
-   }); 
-});
