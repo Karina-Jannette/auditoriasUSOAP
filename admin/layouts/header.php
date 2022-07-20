330c3b4c0bd0a3886f11d4d727e624d73f37f577
Karina-Jannette <103295844+Karina-Jannette@users.noreply.github.com>
2022-07-20 10:15:48 -0500

    carpetas css y más

--- admin/layouts/header.php
+++ admin/layouts/header.php
@@ -15,27 +15,6 @@ include_once ("validar_sesion1.php");
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
         <!-- Navbar Search -->
-        <li class="nav-item">
-            <a class="nav-link" data-widget="navbar-search" href="#" role="button">
-                <i class="fas fa-search"></i>
-            </a>
-            <div class="navbar-search-block">
-                <form class="form-inline">
-                    <div class="input-group input-group-sm">
-                        <input class="form-control form-control-navbar" type="search" placeholder="Search"
-                            aria-label="Search">
-                        <div class="input-group-append">
-                            <button class="btn btn-navbar" type="submit">
-                                <i class="fas fa-search"></i>
-                            </button>
-                            <button class="btn btn-navbar" type="button" data-widget="navbar-search">
-                                <i class="fas fa-times"></i>
-                            </button>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </li>
 
         <!-- Messages Dropdown Menu -->
         <li class="nav-item dropdown">
@@ -177,8 +156,8 @@ include_once ("validar_sesion1.php");
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu">
                 <!-- Add icons to the links using the .nav-icon class
               with font-awesome or any other icon font library -->
-                <li class="nav-item has-treeview menu-open">
-                    <a href="./inicio.php" class="nav-link" aria-pressed="true">
+                <li class="nav-item has-treeview">
+                    <a href="./inicio.php" class="nav-link active">
                         <i class="nav-icon fa fa-home"></i>
                         <p>
                             Inicio
@@ -252,6 +231,10 @@ include_once ("validar_sesion1.php");
         <!-- /.sidebar-menu -->
     </div>
     <!-- /.sidebar -->
+    <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
 </aside>
 
 
